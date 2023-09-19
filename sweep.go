7cc658fd334f76914c3f0ed38a3748d1841f06c8
Pierre Carrier <pc@rrier.fr>
2023-09-19 04:30:32 +0200

    error handling

--- sweep.go
+++ sweep.go
@@ -47,6 +47,10 @@ func main() {
 		newestUploaded := startedAt.Add(-*minAge)
 
 		if err := filepath.WalkDir(*root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
 			if d.IsDir() {
 				return nil
 			}
